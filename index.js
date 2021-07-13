28902b7d5d3e8cea4d153234c27d8b7cec8bbff1
Andy Wang <ci_knight@msn.cn>
2021-07-13 07:15:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const toolCache = require("@actions/tool-cache");
 const path = require("path");
 const fs = require("fs");
 
-const url = "http://gosspublic.alicdn.com/ossutil/1.6.7/ossutil64";
+const url = "http://gosspublic.alicdn.com/ossutil/1.7.1/ossutil64";
 
 async function main() {
   const ENDPOINT = core.getInput("endpoint");
